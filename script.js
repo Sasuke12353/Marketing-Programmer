971952ba2a41fe5f3884c602d39dd7eb831e1a2e
Sasuke12353 <99164372+Sasuke12353@users.noreply.github.com>
2022-07-27 16:00:19 +0300

    mark-pro

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
-let a = 1;
-let b = 1;
+const popupClose = document.querySelector('.popup_close')
+const popupOpen = document.querySelector('.popup_background-dark')
+
+function openPopup() { //открытие popup
+    popup.classList.add('course-card_button text-16 button-grey');
+}
+
+function closePopup() { //закрытие popup
+    popup.classList.remove('popup_close');
+}
+
+editButton.addEventListener('click', openPopup);
+popupClose.addEventListener('click', closePopup);
